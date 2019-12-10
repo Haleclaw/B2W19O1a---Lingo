8fc17ef9508f20fd0f27d3a249fb2e075cd01028
Bram van Ballegooijen <99046330@mydavinci.nl>
2019-12-10 13:45:08 +0100

    added javascript

--- Lingo.js
+++ Lingo.js
@@ -1,11 +1,21 @@
 // Lingo game //
 
-var word =['input']
+
 
 
 function check(){
+	window.letter = (document.querySelector('input[class= "inputTwee"]').value).split('')
+	window.word = (document.querySelector('input[class="input"]').value).split('')
 	
+ 
+
+     for (i=0;i<word.length;i++) {
+        if (letter == word[i]) {
+        document.getElementById("Box"+i).innerHTML = letter[0];
+      
+    }
 }
+	
 
 
-
+}
