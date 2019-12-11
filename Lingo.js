1feb1e342fdc7795cbe810060bd3eefdc3f44909
Bram van Ballegooijen <99046330@mydavinci.nl>
2019-12-11 14:14:56 +0100

    added database, begin letter

--- Lingo.js
+++ Lingo.js
@@ -1,12 +1,14 @@
 // Lingo game //
 
 
-
+var word = words[Math.floor((Math.random() * 479) + 1)]
+document.getElementById("Box0").innerHTML = word[0]
+console.log(word)
 
 function check(){
 	// zorgt ervoor dat het woord in een arry komt //
 	window.letter = (document.querySelector('input[class= "inputTwee"]').value).split('')
-	window.word = (document.querySelector('input[class="input"]').value).split('')
+
 	
  
      // controleer of het goed is // wordt groen //
@@ -14,15 +16,17 @@ function check(){
         if (letter[i] == word[i]) {
         document.getElementById("Box"+i).innerHTML = letter[i];
         document.getElementById("Box"+i).style.backgroundColor = "green"
+        document.getElementById('Box'+i).style.borderRadius = '0px'
     }
      // controleer of de letter er wel in het woord zit // dan word het geel //
 
              else if (word.includes(letter[i])) {
        
              	 document.getElementById("Box"+i).style.backgroundColor = "yellow"
+             	 document.getElementById('Box'+i).style.borderRadius = '90px'
              	 document.getElementById("Box"+i).innerHTML = letter[i];
 
-             	console.log('test')
+             	
              }
                   
 }	
