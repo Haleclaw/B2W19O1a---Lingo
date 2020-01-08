0582c55cbf32de2b18a311b3fdaba0b19fbcb96a
Bram van Ballegooijen <99046330@mydavinci.nl>
2020-01-08 08:51:31 +0100

    fixed boxes

--- Lingo.js
+++ Lingo.js
@@ -1,8 +1,8 @@
 // Lingo game //
 
-
+var x = 0
 var word = words[Math.floor((Math.random() * 479) + 1)]
-document.getElementById("Box0").innerHTML = word[0]
+document.getElementById("Box"+x+"0").innerHTML = word[0]
 console.log(word)
 
 function check(){
@@ -14,22 +14,22 @@ function check(){
      // controleer of het goed is // wordt groen //
      for (i=0;i<word.length;i++) {
         if (letter[i] == word[i]) {
-        document.getElementById("Box"+i).innerHTML = letter[i];
-        document.getElementById("Box"+i).style.backgroundColor = "green"
-        document.getElementById('Box'+i).style.borderRadius = '0px'
+        document.getElementById("Box"+x+i).innerHTML = letter[i];
+        document.getElementById("Box"+x+i).style.backgroundColor = "green"
+        document.getElementById('Box'+x+i).style.borderRadius = '0px'
     }
      // controleer of de letter er wel in het woord zit // dan word het geel //
 
              else if (word.includes(letter[i])) {
        
-             	 document.getElementById("Box"+i).style.backgroundColor = "yellow"
-             	 document.getElementById('Box'+i).style.borderRadius = '90px'
-             	 document.getElementById("Box"+i).innerHTML = letter[i];
+             	 document.getElementById("Box"+x+i).style.backgroundColor = "yellow"
+             	 document.getElementById('Box'+x+i).style.borderRadius = '90px'
+             	 document.getElementById("Box"+x+i).innerHTML = letter[i];
 
              	
              }
                   
 }	
-
+x++
 
 }
